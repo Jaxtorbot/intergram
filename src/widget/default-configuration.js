ac378b2cddb7893ae7ce37a72ab4f5b2c82f3bb0
Jaxtorbot <91402178+Jaxtorbot@users.noreply.github.com>
2021-09-26 21:51:43 -0300

    Update default-configuration.js

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -5,13 +5,13 @@ export const defaultConfiguration = {
     closedStyle: 'chat', // button or chat
     closedChatAvatarUrl: '', // only used if closedStyle is set to 'chat'
     cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
-    introMessage: 'Hello! How can we help you?',
-    autoResponse: 'Looking for the first available admin (It might take a minute)',
-    autoNoResponse: 'It seems that no one is available to answer right now. Please tell us how we can ' +
-    'contact you, and we will get back to you as soon as we can.',
-    placeholderText: 'Send a message...',
+    introMessage: 'Ol&aacute;! Em que posso te ajudar?',
+    autoResponse: 'Procurando atendente. (Isso pode demorar um pouquinho.)',
+    autoNoResponse: 'Parece que n&atilde;o temos atendentes dispon&iacute;veis nesse momento. Por favor nos diga como' +
+    'Nos diga como podemos entrar em contato, entraremos em contato assim que poss&iacute;vel.',
+    placeholderText: 'Envie uma mensagem...',
     displayMessageTime: true,
-    mainColor: '#1f8ceb',
+    mainColor: '#11ABB0',
     alwaysUseFloatingButton: false,
     desktopHeight: 450,
     desktopWidth: 370
